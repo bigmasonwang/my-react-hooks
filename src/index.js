ab71ce675bca030ed9ee4eda608af43e9d99f59e
Mason Wang <bigmasonwang@gmail.com>
2023-01-26 23:53:28 +1030

    try 3

--- src/index.js
+++ src/index.js
@@ -1,24 +1,29 @@
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
 
-let _state;
+const states = [];
+let index = 0;
 const useMyState = (initialState) => {
-  _state = _state || initialState;
-
+  const state = states[index] || initialState;
+  // need to freeze current hook's index
+  const currentIndex = index;
   const setState = (newState) => {
-    _state = newState;
+    states[currentIndex] = newState;
     renderApp();
   };
-
-  return [_state, setState];
+  index++;
+  return [state, setState];
 };
 
 const App = () => {
   const [count, setCount] = useMyState(0);
+  const [count2, setCount2] = useMyState(0);
   return (
     <div>
       <div>{count}</div>
       <button onClick={() => setCount(count + 1)}>+</button>
+      <div>{count2}</div>
+      <button onClick={() => setCount2(count2 + 2)}>+</button>
     </div>
   );
 };
@@ -26,6 +31,7 @@ const App = () => {
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 const renderApp = () => {
+  index = 0;
   root.render(
     <React.StrictMode>
       <App />
