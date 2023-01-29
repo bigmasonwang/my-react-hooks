be60e6fceda88e278a2b1eeb9e4151e3c523b789
Mason Wang <bigmasonwang@gmail.com>
2023-01-29 19:21:07 +1030

    child render if without memo

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { memo, useCallback, useMemo, useState } from 'react';
 import ReactDOM from 'react-dom/client';
 
 const hookStates = [];
@@ -7,8 +7,9 @@ const useMyState = (initialState) => {
   const isFirstRender = hookStates[hookIndex] === undefined;
   const state = isFirstRender ? initialState : hookStates[hookIndex];
 
-  // need to freeze current hook's index
+  // need to freeze current hook's index, the variable in closure
   const currentIndex = hookIndex;
+
   const setState = (newState) => {
     hookStates[currentIndex] = newState;
     renderApp();
@@ -17,14 +18,43 @@ const useMyState = (initialState) => {
   return [state, setState];
 };
 
+const useMyCallback = (callback, dependencies) => {
+  if (hookStates[hookIndex]) {
+    const [lastCallback, lastDependencies] = hookStates[hookIndex];
+    const isSame = dependencies.every(
+      (dependency, index) => dependency === lastDependencies[index]
+    );
+    if (isSame) {
+      hookIndex++;
+      return lastCallback;
+    }
+    hookStates[hookIndex] = [callback, dependencies];
+    hookIndex++;
+    return callback;
+  }
+  // first render
+  hookStates[hookIndex] = [callback, dependencies];
+  hookIndex++;
+  return callback;
+};
+
+const Child = ({ value, onClick }) => {
+  console.log('Child component render');
+  return <button onClick={onClick}>{value}</button>;
+};
+// will not rerender if props not change
+const MemorisedChild = memo(Child);
+
 const App = () => {
   const [count, setCount] = useMyState(0);
   const [input, setInput] = useMyState('mason');
 
+  const handleButtonClick = () => setCount(count + 1);
+
   return (
     <div>
       <div>
-        <button onClick={() => setCount(count + 1)}>{count}</button>
+        <MemorisedChild value={count} onClick={handleButtonClick} />
       </div>
       <div>
         <input
